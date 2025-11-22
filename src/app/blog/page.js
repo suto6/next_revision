67b2ef4ee540bdbea190e57c78d7435b30bd3feb
Suto <sutapacreator@gmail.com>
2025-11-22 19:24:31 +0530

    1st

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,3 +1,20 @@
 export default function BlogPage() {
-  return <div>Blog</div>;
+    const posts = [
+      { id: "1", title: "Post One", body: "This is the body of post one." },
+      { id: "2", title: "Post Two", body: "This is the body of post two." },
+      { id: "3", title: "Post Three", body: "This is the body of post three." }
+    ]
+  return <div>
+    <h1>Blog Page</h1>
+    <div>
+        {posts.map((post)=>{
+            return (
+                <>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </>
+            )
+        })}
+    </div>
+  </div>;
 }
